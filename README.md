1f358f208c75f0b421c1b89088b1deafa6252d0e
gyoforit <gyoforit@gmail.com>
2021-01-19 18:43:31 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# study_algorithm
+
+알고리즘 뿌수기 👊 solved.ac(백준) / 프로그래머스 / SWEA 문제 풀이
---
+++ solvedac/210119_solvedac.py
@@ -0,0 +1,16 @@
+# solved.ac Bronze5
+# 1000 A+B
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
+
+# 1001 A-B
+
+# 1271 엄청난 부자2
+
+# 1550 16진수
+
+# 2338 긴자리 계산
+
+# 2475 검증수
