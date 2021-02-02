49babf2e6070849ce8c669649665ae0445703199
gyoforit <gyoforit@gmail.com>
2021-02-02 23:27:43 +0900

    solving #1928

--- SWEA/D2/swea_d2.py
+++ SWEA/D2/swea_d2.py
@@ -1,30 +1,30 @@
 # 210202
 # 1945. 간단한 소인수분해
-T = int(input())
-for i in range(1, T+1):
-    a = 0
-    b = 0
-    c = 0
-    d = 0
-    e = 0
-    N = int(input())
-    while N > 1:
-        if N % 11 == 0:
-            e += 1
-            N = N//11
-        elif N % 7 == 0:
-            d += 1
-            N = N//7
-        elif N % 5 == 0:
-            c += 1
-            N = N//5
-        elif N % 3 == 0:
-            b += 1
-            N = N//3
-        elif N % 2 == 0:
-            a += 1
-            N = N//2
-    print(f"#{i} {a} {b} {c} {d} {e}")
+# T = int(input())
+# for i in range(1, T+1):
+#     a = 0
+#     b = 0
+#     c = 0
+#     d = 0
+#     e = 0
+#     N = int(input())
+#     while N > 1:
+#         if N % 11 == 0:
+#             e += 1
+#             N = N//11
+#         elif N % 7 == 0:
+#             d += 1
+#             N = N//7
+#         elif N % 5 == 0:
+#             c += 1
+#             N = N//5
+#         elif N % 3 == 0:
+#             b += 1
+#             N = N//3
+#         elif N % 2 == 0:
+#             a += 1
+#             N = N//2
+#     print(f"#{i} {a} {b} {c} {d} {e}")
 
 # 1928. Base64 Decoder
 
@@ -38,6 +38,7 @@ for i in range(1, T+1):
 
 # 이걸 거꾸로 해야됨...^^
 # format, rjust 배워서 써먹기!
+
 import base64
 
 N = 'Man'
@@ -61,10 +62,15 @@ for i in range(len(binarylist)//6 -1):
     chars6int += '0b' + ''.join(chars6)
     x = str(int(chars6int, 2))
 
+# ascii 이용하여 index 값 구하기
+# index를 이진수로 바꿔서 나열하기
+# 8개씩 slicing해서 10진수로 변환
+# ascii 이용하여 다시 변환
 
 N = 'TWFu'
 for char in N:
     print(ord(char))
+    
 
 
 # 1986. 지그재그 숫자
