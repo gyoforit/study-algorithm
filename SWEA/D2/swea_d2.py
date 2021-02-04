6d292ff544fc03933b8402815f6336c76ebf834a
gyoforit <gyoforit@gmail.com>
2021-02-04 22:27:28 +0900

    <Feb 1 day 1 algorithm_day4> 210203 SWEA D2 #1986 1284 1989

--- SWEA/D2/swea_d2.py
+++ SWEA/D2/swea_d2.py
@@ -1,30 +1,30 @@
 # 210202
 # 1945. 간단한 소인수분해
-# T = int(input())
-# for i in range(1, T+1):
-#     a = 0
-#     b = 0
-#     c = 0
-#     d = 0
-#     e = 0
-#     N = int(input())
-#     while N > 1:
-#         if N % 11 == 0:
-#             e += 1
-#             N = N//11
-#         elif N % 7 == 0:
-#             d += 1
-#             N = N//7
-#         elif N % 5 == 0:
-#             c += 1
-#             N = N//5
-#         elif N % 3 == 0:
-#             b += 1
-#             N = N//3
-#         elif N % 2 == 0:
-#             a += 1
-#             N = N//2
-#     print(f"#{i} {a} {b} {c} {d} {e}")
+T = int(input())
+for i in range(1, T+1):
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
+    N = int(input())
+    while N > 1:
+        if N % 11 == 0:
+            e += 1
+            N = N//11
+        elif N % 7 == 0:
+            d += 1
+            N = N//7
+        elif N % 5 == 0:
+            c += 1
+            N = N//5
+        elif N % 3 == 0:
+            b += 1
+            N = N//3
+        elif N % 2 == 0:
+            a += 1
+            N = N//2
+    print(f"#{i} {a} {b} {c} {d} {e}")
 
 # 1928. Base64 Decoder
 
@@ -39,44 +39,94 @@
 # 이걸 거꾸로 해야됨...^^
 # format, rjust 배워서 써먹기!
 
-import base64
+# import base64
 
-N = 'Man'
-binarychars = ''
-binarylist = []
-for char in N:
-    bin_char = ''
-    asc = ord(char)
-    bin_char += bin(asc)[2:]
-    while len(bin_char) < 8:
-        bin_char = '0' + bin_char
-        binarychars += bin_char
-# 01001101011... 출력
-# # print(type(binarychars))
-binarylist.extend(binarychars)
-for i in range(len(binarylist)//6 -1):
-    chars6 = []
-    chars6int = ''
-    chars6 += binarylist[6*i:6*(i+1)]
-    # print(''.join(chars6))
-    chars6int += '0b' + ''.join(chars6)
-    x = str(int(chars6int, 2))
+# N = 'Man'
+# binarychars = ''
+# binarylist = []
+# for char in N:
+#     bin_char = ''
+#     asc = ord(char)
+#     bin_char += bin(asc)[2:]
+#     while len(bin_char) < 8:
+#         bin_char = '0' + bin_char
+#         binarychars += bin_char
+# # 01001101011... 출력
+# # # print(type(binarychars))
+# binarylist.extend(binarychars)
+# for i in range(len(binarylist)//6 -1):
+#     chars6 = []
+#     chars6int = ''
+#     chars6 += binarylist[6*i:6*(i+1)]
+#     # print(''.join(chars6))
+#     chars6int += '0b' + ''.join(chars6)
+#     x = str(int(chars6int, 2))
 
 # ascii 이용하여 index 값 구하기
 # index를 이진수로 바꿔서 나열하기
 # 8개씩 slicing해서 10진수로 변환
 # ascii 이용하여 다시 변환
 
-N = 'TWFu'
-for char in N:
-    print(ord(char))
+# N = 'TWFu'
+# for char in N:
+#     print(ord(char))
     
-
-
 # 1986. 지그재그 숫자
+T = int(input())
+for i in range(1, T+1):
+    N = int(input())
+    result = 0
+    for n in range(1, N+1):
+        if n % 2:
+            result += n
+        else:
+            result -= n
+    print(f"#{i} {result}")
+
 # 1284. 수도 요금 경쟁
+T = int(input())
+for i in range(1, T+1):
+    numbers = list(map(int, input().split()))
+    A = 0
+    B = 0
+    result = 0
+    A = numbers[0]*numbers[4] # P*W
+    if numbers[4] <= numbers[2]: # W<=R
+        B = numbers[1] # Q
+    else:
+        B = numbers[1] + numbers[3]*(numbers[4]-numbers[2])
+    result = min(A, B)
+    print(f"#{i} {result}")
+# 문제 제대로 읽기!!! 최소요금!!!
+
 # 1288. 새로운 불면증 치료법
+T = int(input())
+for i in range(1, T+1):
+    N = int(input()) #1
+    numbers = set()
+    while len(numbers) < 10:
+        count = 0
+        count += 1
+        N = N*count
+        num = []
+        num.extend(str(N))
+        for idx in range(len(num)):
+            numbers.add(int(num[idx]))
+    print(count)
+
 # 1989. 초심자의 회문 검사
+T = int(input())
+for i in range(1, T+1):
+    chars = input()
+    while len(chars) < 2:
+        if chars[0] != chars[-1]:
+            print(f"#{i} 0")
+            break
+        else:
+            chars = chars[1:-1]
+    print(f"#{i} 1")
+# if문을 while문 안에?!
+
 # 1959. 두 개의 숫자열
 # 1948. 날짜 계산기
 # 2007. 패턴 마디의 길이
