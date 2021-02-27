9e507db0d8b67aeb0a8b256754cafde23555946c
gyoforit <gyoforit@gmail.com>
2021-02-28 00:50:06 +0900

    <Feb 1day 1algorithm_day27> 210227 1 more problem

--- baekjoon/step6/boj_step6.py
+++ baekjoon/step6/boj_step6.py
@@ -35,3 +35,27 @@ N = int(input())
 for n in range(1, N+1):
     # 99까진 생략
     nums = list(map(int, str(n))) # 자릿수대로 끊어짐!!
+
+# 4673. 셀프 넘버
+d_list = []
+# 셀프 넘버가 아닌 수들을 append하는 함수 (재귀 사용)
+def d(N):
+    if N > 10000:
+        return
+    else:
+        result = N
+        N_list = list(map(int, str(result)))
+        result += sum(N_list)
+        if result <= 10000:
+            d_list.append(result)
+        return d(result)
+
+# d 함수 사용해서 셀프 넘버가 아닌 수들을 모두 append
+for i in range(1, 10001):
+    if i not in d_list:
+        d(i)
+
+# d_list에 없는 수들만 print
+for j in range(1, 10001):
+    if j not in d_list:
+        print(j)
