9b7840411bfe8e572d2a4808c5be78f20c019124
gyoforit <gyoforit@gmail.com>
2021-03-07 22:53:03 +0900

    1 day 1 algorithm_210307: 7 problems

--- baekjoon/etc/boj_etc.py
+++ baekjoon/etc/boj_etc.py
@@ -45,3 +45,46 @@ for _ in range(4):
             result = 'b'
 
     print(result)
+
+# 210307 - 별찍기 시리즈
+# 2440. 별찍기 - 3
+N = int(input())
+for i in range(N, 0, -1):
+    print("*"*i)
+
+# 2441. 별찍기 - 4
+N = int(input())
+for i in range(N, 0, -1):
+    print((' '*(N-i))+'*'*(i))
+
+# 2442. 별찍기 - 5
+N = int(input())
+for i in range(1, N+1):
+    print((' '*(N-i))+('*'*(2*i-1)))
+
+# 2443. 별찍기 - 6
+N = int(input())
+for i in range(N, 0, -1):
+    print((' '*(N-i))+('*'*(2*i-1)))
+
+# 2444. 별찍기 - 7
+N = int(input())
+for i in range(1, 2*N):
+    if i <= N:
+        print((' '*(N-i))+('*'*(2*i-1)))
+    else: # 6, 7, 8, 9
+        # tmp = abs(N-i) # i-N
+        # print((' '*(i-N))+('*'*(2*(N-tmp)-1)))
+        print((' ' * (i-N)) + ('*'*(2*(2*N-i)-1)))
+
+# 2445. 별찍기 - 8
+N = int(input())
+for i in range(1, 2*N):
+    x = abs(N-i)
+    print(('*'*(N-x))+(' '*(2*x))+('*'*(N-x)))
+
+# 2446. 별찍기 - 9
+N = int(input())
+for i in range(1,2*N):
+    x = abs(N-i)
+    print((' '*(N-x-1))+('*'*(2*x+1)))
