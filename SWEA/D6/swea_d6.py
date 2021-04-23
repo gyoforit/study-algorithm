d8bc911070378efd5591c04d98e4610381cfd0f5
gyoforit <gyoforit@gmail.com>
2021-04-23 22:56:56 +0900

    SWEA 1795. 인수의 생일 파티

---
+++ SWEA/D6/swea_d6.py
@@ -0,0 +1,33 @@
+# 210423
+# 1795. 인수의 생일 파티
+INF = 987654321
+def dijkstra(x, matrix):
+    visited = [0]*(N+1)
+    D = [INF]*(N+1)
+    visited[x] = 1
+    D[x] = 0
+    for i in range(1, N+1):
+        if matrix[x][i]: D[i] = matrix[x][i]
+    for _ in range(N-1):
+        mn = INF
+        for i in range(1, N+1):
+            if D[i] < mn and not visited[i]:
+                mn, mn_idx = D[i], i
+        visited[mn_idx] = 1
+        for i in range(1, N+1):
+            if matrix[mn_idx][i]: D[i] = min(D[i], D[mn_idx]+matrix[mn_idx][i])
+    return D
+
+T = int(input())
+for t in range(1, T+1):
+    N, M, X = map(int, input().split())
+    AM = [[0]*(N+1) for _ in range(N+1)]
+    BM = [[0]*(N+1) for _ in range(N+1)]
+    for _ in range(M):
+        s, e, c = map(int, input().split())
+        AM[s][e] = c
+        BM[e][s] = c
+    go = dijkstra(X, AM)
+    back = dijkstra(X, BM)
+    result = max([go[i]+back[i] for i in range(1, N+1)])
+    print("#%d %d" % (t, result))
