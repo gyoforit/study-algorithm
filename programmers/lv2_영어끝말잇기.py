5d4abd2ec2419f3d0b779b7f8ed24877520c11a2
gyoforit <gyoforit@gmail.com>
2021-07-27 21:18:28 +0900

    프로그래머스 lv2. 영어 끝말잇기 (수정)

--- programmers/lv2_영어끝말잇기.py
+++ programmers/lv2_영어끝말잇기.py
@@ -1,12 +1,12 @@
-# set 사용해서 있는지 체크
-# 몇번째 사람인지는 인덱스+1 을 n으로 나눈 나머지 (0이면 n)
-# 차례는 인덱스를 n으로 나눈 몫+1
+# set 사용해서 중복 체크
+# 사람: 인덱스+1 을 n으로 나눈 나머지 (0이면 n)
+# 차례: 인덱스를 n으로 나눈 몫+1
+# for-else 문: for문을 break 없이 끝까지 돌면 else문 탐
 def solution(n, words):
     answer = []
     already = set()
     before = words[0]
     already.add(before)
-    flag = 0
     for i in range(1, len(words)):
         now = words[i]
         if now not in already and before[-1] == now[0]:
@@ -16,8 +16,7 @@ def solution(n, words):
             person = (i+1)%n if (i+1)%n else n
             answer.append(person)
             answer.append((i//n)+1)
-            flag = 1
             break
-    if not flag:
+    else:
         answer = [0, 0]
     return answer
