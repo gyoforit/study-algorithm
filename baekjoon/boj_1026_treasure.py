08636f721bf85430d08d3d72a4d6ad457cf45583
gyoforit <gyoforit@gmail.com>
2021-10-19 22:18:57 +0900

    백준 1026. 보물

---
+++ baekjoon/boj_1026_treasure.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open('input.txt')
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+A.sort()
+B.sort(reverse=True)
+print(sum([A[i]*B[i] for i in range(N)]))
